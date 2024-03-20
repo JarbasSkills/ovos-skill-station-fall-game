7e88303d9d9e68d212a8ea8fe3820e59f8232bee
miro <jarbasai@mailfence.com>
2024-03-20 19:09:56 +0000

    modernize

--- __init__.py
+++ __init__.py
@@ -1,23 +1,15 @@
 from ovos_workshop.decorators import conversational_intent, intent_handler
 from pyfrotz.ovos import FrotzSkill
+from pyfrotz.parsers import stationfall_intro_parser
 
 
 class StationFallSkill(FrotzSkill):
     def __init__(self, *args, **kwargs):
-        # Game specific init
-        def intro_parser(game):
-            intro = """it's been five years since your planetfall on resida.
-                    your heroics in saving that doomed world resulted in a big promotion, but your life of dull scrubwork has been replaced by a life of dull paperwork.
-                    today you find yourself amidst the administrative maze of deck twelve on a typically exciting task: an emergency mission to space station gamma delta gamma 777- g 59/59 sector alpha-mu-79 to pick up a supply of request for stellar patrol issue regulation black form binders request form forms
-                    """
-            game._frotz_read()
-            return intro
-
         # game is english only, apply bidirectional translation
         super().__init__(game_id="stationfall",
                          game_lang="en-us",
                          game_data=f'{self.root_dir}/res/{self.game_id}.DAT',
-                         intro_parser=intro_parser,
+                         intro_parser=stationfall_intro_parser,
                          *args, **kwargs)
 
     @intent_handler("play.intent")
